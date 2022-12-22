bd7bff705c123a0b5a00ee98b420c8122ea166e4
Alex Migliori <107506052+AlexMigliori@users.noreply.github.com>
2022-12-22 15:02:49 -0700

    Update App.jsx

--- react-dice/src/App.jsx
+++ react-dice/src/App.jsx
@@ -9,10 +9,7 @@ export default function App() {
     const populateNumbers = () => {
         return Math.floor(Math.random() * 5) + 1
     }
-    //This allows for synchronous execution
-    // useEffect(() => {
-    //   console.log(numbers)
-    // })
+  
 
     const rollDice = () => {
         const diceRollingSound = new Audio(diceRolling)
